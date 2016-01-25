91275f8ab6a45d4dd74b2ef3aa5f2297549be1d8
Your Name <goraniliev93@yahoo.com>
2016-01-25 01:07:49 +0100

    Show clusters functionality added.

--- TwitterUsersClustering.py
+++ TwitterUsersClustering.py
@@ -1,7 +1,8 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 from api.API import get_api_instance
 from clustering.kmedoids import get_clusters, update_clusters_in_db
 from db.Insert_users import insert_users_from_time_to_db, set_auto_increment_keys_for_already_inserted_users
+from db.get_utilities import get_clusters_from_db
 
 app = Flask(__name__)
 
@@ -43,10 +44,11 @@ def update_clusters():
 
 @app.route('/show_clusters', methods=['GET', 'POST'])
 def show_clusters():
-    clusters = get_clusters()
-    for c in clusters:
-        print len(clusters[c])
-    return app.send_static_file('main.html')
+    clusters = get_clusters_from_db()
+    # for c in clusters:
+    #     print c, len(clusters[c])
+    return render_template('clusters.html', title='Clusters', clusters=clusters)
+    # return app.send_static_file('main.html')
 
 
 if __name__ == '__main__':
--- db/get_utilities.py
+++ db/get_utilities.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from collections import OrderedDict
 from db.Insert_users import get_connection
 
 __author__ = 'goran'
@@ -41,3 +42,26 @@ def get_followers():
     db.close()
 
     return followers
+
+
+def get_clusters_from_db():
+    db = get_connection()
+    cursor = db.cursor()
+
+    sql = """select screenname, cluster from user"""
+
+    cursor.execute(sql)
+
+    clusters = {}
+
+    for (screen_name, cluster) in cursor:
+        clusters.setdefault(cluster, [])
+        clusters[cluster].append(screen_name)
+
+    idx = 1
+    ordered_clusters = OrderedDict()
+    for cluster, users in clusters.iteritems():
+        ordered_clusters['Cluster ' + str(idx)] = users
+        idx += 1
+
+    return ordered_clusters
